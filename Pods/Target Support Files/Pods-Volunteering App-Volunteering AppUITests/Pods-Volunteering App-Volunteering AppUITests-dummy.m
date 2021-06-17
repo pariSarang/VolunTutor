02de09b5397f8c68a0333227d4ef518ab4bf9801
Devochska Sarangdevot <pari_s@icloud.com>
2021-06-17 15:46:42 -0500

    Adding the volunteering app

---
+++ Pods/Target Support Files/Pods-Volunteering App-Volunteering AppUITests/Pods-Volunteering App-Volunteering AppUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Volunteering_App_Volunteering_AppUITests : NSObject
+@end
+@implementation PodsDummy_Pods_Volunteering_App_Volunteering_AppUITests
+@end
---
+++ Pods/Target Support Files/Pods-Volunteering App/Pods-Volunteering App-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_Volunteering_App : NSObject
+@end
+@implementation PodsDummy_Pods_Volunteering_App
+@end
